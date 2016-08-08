aa84d71e2fde26aebbd3eade2fc2519203e702e2
Riley <tay0008@jmss.vic.edu.au>
2016-08-08 17:03:49 +1000

    Updated joke system

--- metabot.py
+++ metabot.py
@@ -233,36 +233,8 @@ if __name__ == "__main__":
         "Oh, hello X!", "Ladies and Gentlemen, it is my great pleasure today to introduce X!",
         "Welcome to the gamemaker slack, X.", "Welcome, X!", "Ooh we have a new person.\nHello X!"
     ]
-    JOKES = [
-	"There are only 10 types of people in the world: those that understand binary and those that don’t.",
-	"Failure is not an option. It comes bundled with your Microsoft product.",
-	"I’m not anti-social; I’m just not user friendly.",
-	"My attitude isn’t bad. It’s in beta.",
-	"Latest survey shows that 3 out of 4 people make up 75% of the world’s population.",
-	"What do you get when you cross a cow and a trampoline? A milkshake.",
-	"Mother, “How was school today, Patrick?”\n\nPatrick, “It was really great mum! Today we made explosives!”\n\nMother, “Ooh, they do very fancy stuff with you these days. And what will you do at school tomorrow?”\n\nPatrick, “What school?”",
-	'Police officer: "Can you identify yourself, sir?"\n\nDriver pulls out his mirror and says: "Yes, '+"it's me."+'"',
-	"I can’t believe I forgot to go to the gym today. That’s 7 years in a row now.",
-	"A naked woman robbed a bank. Nobody could remember her face.",
-	"The 21st century: Deleting history is often more important than making it.",
-	"Woke up with a dead leg this morning. I will not take out a loan with the mafia ever again.",
-	"Why do cows wear bells?\n\nTheir horns don’t work.",
-	"Why haven’t you ever seen any elephants hiding up trees? Because they’re really, really good at it.",
-	"I asked my North Korean friend how it was to live in North Korea. He said he can't complain.",
-	"Q: How many programmers does it take to change a light bulb?\n\nA: None, that's a hardware problem.",
-	"Where's the best place to hide a body?\n\nPage two of Google.",
-	"I love pressing F5. It's so refreshing.",
-        "Anyone can build a bridge that doesn't fall down. Only an engineer can build a bridge that just barely doesn't fall down.",
-        'Three statisticians go hunting. They see a deer and the first one shoots, hitting three feet left of the deer. The second one shoots, hitting three feet right of the deer. The third one leaps up in joy, yelling, "we got him!"',
-        "Standard Deviation not enough for Perverted Statistician",
-        "Any salad is a Caesar salad if you stab it enough",
-        "If I got $1 every time somebody called me a racist... Black people would rob me",
-        "I have an EpiPen. My friend gave it to me as he was dying. It seemed very important to him that I have it.",
-        "Why are gay men so well dressed?\n\nThey didn't spend all that time in the closet doing nothing.",
-        "What's a pirate's least favourite letter?\n\n```Dear Sir,\nWe are writing to you because you have violated copyright...```",
-        "I started a company selling land mines that look like prayer mats. Prophets are going through the roof.",
-        'So a gorilla dies at a zoo right before the zoo opens. It is the only gorilla at the zoo since they are not very profitable. However, the gorilla is their most popular attraction by far, and they cannot afford to go a day without it. So the zoo owner asks one of his workers to wear a gorilla suit they have in storage for an extra $100 a day if he will go in the gorilla cage and pretend to be the gorilla until the zoo can afford a new one.\n\nQuickly, the new "gorilla" becomes the most popular craze at the zoo. People from all over are coming to see the "Human-like" gorilla. About a month in, the craze has started to wear off. So, to get peoples attention back, he decides to climb over his enclosure and hang from the net ceiling above the lions den next to him. A large crowd of people gather watching the spectacle in awe and terror. Suddenly the man loses his grip and falls to the floor of the lions den. The man starts screaming "HELP!! HELP!!!" Suddenly a lion pounces him from behind and whispers in his ear, "Shut the fuck up right now or you'+"'"+'re going to get us both fired."'
-    ]
+    with open('jokes.txt') as file:
+        JOKES = file.read().split('\n')
 
     schedule.every().day.at('19:30').do(check_studio_update)
 
